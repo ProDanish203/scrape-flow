e09d52238e762f014843186b8798e9e4ce5843f6
Danish <danishsidd524@gmail.com>
2025-01-27 01:28:34 +0500

    add: ai data extraction

--- lib/data.ts
+++ lib/data.ts
@@ -27,3 +27,6 @@ export const dashboardRoutes = [
     icon: CoinsIcon,
   },
 ];
+
+export const SystemPrompt =
+  "You are a web scraper helper that extract data from the provided HTML or text. You will be given a piece of text or HTML content as input and also the prompt with the data you have to extract. the response should always be only the extracted data as a JSON array or object, without any additional words or explanation. analyze the input carefully and extract data precisely based on the prompt. If no data is found, return an empty JSON array. work only with the provided content and ensure the output is always a valid JSON array without any surrounding text.";
--- lib/workflow/executor/ExtractDataWithAiExecutor.ts
+++ lib/workflow/executor/ExtractDataWithAiExecutor.ts
@@ -2,6 +2,10 @@ import { ExecutionEnvironment } from "@/types/executor";
 import { ExtractDataWithAiTask } from "../task/ExtractDataWithAI";
 import prisma from "@/lib/prisma";
 import { symmetricDecrypt } from "@/lib/encryption";
+// import OpenAi from "openai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { SystemPrompt } from "@/lib/data";
+import { extractJSON } from "@/lib/helper/ai";
 
 export async function ExtractDataWithAiExecutor(
   environment: ExecutionEnvironment<typeof ExtractDataWithAiTask>
@@ -35,13 +39,93 @@ export async function ExtractDataWithAiExecutor(
       return false;
     }
 
-    const mockExtractedData = {
-      usernameSelector: "#username",
-      passwordSelector: "#password",
-      loginSelector: "body > div > form > input.btn.btn-primary",
+    // const mockExtractedData = {
+    //   usernameSelector: "#username",
+    //   passwordSelector: "#password",
+    //   loginSelector: "body > div > form > input.btn.btn-primary",
+    // };
+
+    // environment.setOutput("Extracted data", JSON.stringify(mockExtractedData));
+
+    // // Open Ai Implementation
+    // const openai = new OpenAi({
+    //   apiKey: decryptedCredentialValue,
+    // });
+
+    // const response = await openai.chat.completions.create({
+    //   model: "gpt-4o-mini",
+    //   messages: [
+    //     {
+    //       role: "system",
+    //       content: SystemPrompt,
+    //     },
+    //     {
+    //       role: "user",
+    //       content: content,
+    //     },
+    //     {
+    //       role: "user",
+    //       content: prompt,
+    //     },
+    //   ],
+    //   temperature: 1,
+    //   // max_tokens: 500,
+    // });
+
+    // environment.log.info(
+    //   `Prompt tokens used: ${response.usage?.prompt_tokens}`
+    // );
+    // environment.log.info(
+    //   `Completion tokens used: ${response.usage?.completion_tokens}`
+    // );
+
+    // const result = response.choices[0].message?.content;
+
+    // // Gemini Implementation
+    const gemini = new GoogleGenerativeAI(decryptedCredentialValue);
+    const model = gemini.getGenerativeModel({
+      model: "gemini-1.5-flash",
+      systemInstruction: SystemPrompt,
+    });
+
+    const generationConfig = {
+      temperature: 1,
+      topP: 0.95,
+      topK: 40,
+      maxOutputTokens: 8192,
+      responseMimeType: "text/plain",
     };
 
-    environment.setOutput("Extracted data", JSON.stringify(mockExtractedData));
+    const response = await model.generateContent({
+      contents: [
+        {
+          role: "user",
+          parts: [{ text: content }],
+        },
+        {
+          role: "user",
+          parts: [{ text: prompt }],
+        },
+      ],
+      generationConfig,
+    });
+
+    environment.log.info(
+      `Prompt tokens used: ${response.response.usageMetadata?.promptTokenCount}`
+    );
+    environment.log.info(
+      `Completion tokens used: ${response.response.usageMetadata?.candidatesTokenCount}`
+    );
+
+    const result = response.response.text();
+
+    if (!result) {
+      environment.log.error("Failed to get the response from AI");
+      return false;
+    }
+
+    const parsedJson = extractJSON(result);
+    environment.setOutput("Extracted data", JSON.stringify(parsedJson));
 
     return true;
   } catch (err: any) {
